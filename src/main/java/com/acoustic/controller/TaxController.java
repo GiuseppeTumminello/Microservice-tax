367dd94670aa3cea6cbd6e5bbdfba5358f278dc2
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-15 10:33:26 +0200

    changed properties adding NATIVE

--- src/main/java/com/acoustic/controller/TaxController.java
+++ src/main/java/com/acoustic/controller/TaxController.java
@@ -19,12 +19,13 @@ public class TaxController {
 
     private static final String DESCRIPTION = "description";
     private static final String VALUE = "value";
+    public static final int MINIMUM_GROSS = 2000;
     private final TaxRepository taxRepository;
     private final SalaryCalculatorService salaryCalculatorService;
 
 
     @PostMapping("/calculation/{grossMonthlySalary}")
-    public Map<String, String> calculateTax(@PathVariable @Min(2000) BigDecimal grossMonthlySalary) {
+    public Map<String, String> calculateTax(@PathVariable @Min(MINIMUM_GROSS) BigDecimal grossMonthlySalary) {
         var tax = this.salaryCalculatorService.apply(grossMonthlySalary);
         this.taxRepository.save(Tax.builder().taxAmount(tax).build());
         return Map.of(DESCRIPTION,this.salaryCalculatorService.getDescription(), VALUE, String.valueOf(tax));
